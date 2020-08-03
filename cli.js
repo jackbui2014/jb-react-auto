5f9b165fc5ca2d8cba5306012617a7519c05216e
Bui Hieu Tam <tamb@SCCs-MacBook-Pro.local>
2020-08-03 17:23:11 +0700

    dev

---
+++ cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+//Grab provided args
+const [,, ...args] = process.argv
+
+const c = require('./index');
+const allowArray = ['component', 'page', 'layout'];
+if( typeof args['0'] !== 'undefined'){
+	if( allowArray.indexOf(args['0'].toLowerCase()) != -1 ){
+		if( typeof args['1'] !== 'undefined'){
+			c.createComponent('src/'+args['0']+'s'+'/', args['1']);
+		}
+		else{
+			console.log('Error!');
+			return false;
+		}
+		if( args['0'].toLowerCase() == 'page'){
+			c.createRoute(args['1']);
+		}
+		return true;
+	}
+}
+console.log('Error!');
+return false;
---
+++ index.js
@@ -0,0 +1,74 @@
+'use strict';
+var handlebars = require('handlebars');
+var fs = require('fs'),
+	path = require('path');
+const createDir = (dirPath)=>{
+	fs.mkdirSync(process.cwd()+dirPath, { recursive: true}, (error)=>{
+		if( error){
+			console.log('error');
+		}
+		else{
+			console.log('created a directory');
+		}
+	});
+}
+exports.createDir = createDir;
+const createFile = (filePath, fileContent) => {
+	fs.writeFile(filePath, fileContent, (error)=>{
+		if( error){
+			console.log('error');
+		}
+		else{
+			console.log('created a file');
+		}
+	});
+}
+exports.createFile = createFile;
+
+// this will be called after the file is read
+const renderToString = (source, data) => {
+  var template = handlebars.compile(source);
+  var outputString = template(data);
+  return outputString;
+}
+exports.renderToString = renderToString;
+
+const createFileFromTemplate = (templatePath, FilePath, componentName) =>{
+	 var jsonData = {
+	 	'name': componentName
+	 };
+	// read the file and use the callback to render
+	fs.readFile(templatePath, function(err, data){
+		console.log(err);
+	  if (!err) {
+	    // make the buffer into a string
+	    var source = data.toString();
+	    // call the render function
+	    var mainContent =  renderToString(source, jsonData);
+	    createFile(FilePath+componentName+'/'+componentName+'.jsx', mainContent);
+	    return true;
+	  } else {
+	    return false;
+	  }
+	});
+
+}
+exports.createFileFromTemplate = createFileFromTemplate;
+
+const createComponent = (dir, componentName)=>{
+	console.log(dir);
+	console.log(componentName);
+	createDir('/'+dir+componentName);
+	createFileFromTemplate(path.join(__dirname, '/templates/maincomponentjsfile.hbs'), dir, componentName);
+	var pckContent = '{ "main": "'+componentName+'.jsx" } ';
+	createFile(dir+componentName+'/package.json', pckContent);
+	createFile(dir+componentName+'/'+componentName+'.scss', '');
+	createDir('/'+dir+componentName+'/__Test__');
+	createFile(dir+componentName+'/__Test__/'+componentName+'.js', '');
+	
+}
+exports.createComponent = createComponent;
+const createRoute = (componentName) => {
+	createFileFromTemplate(path.join(__dirname, '/templates/route.hbs'), 'src/routes/', componentName+'s');
+}
+exports.createRoute = createRoute;
