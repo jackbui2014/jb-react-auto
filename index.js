6022cb3832629357c076cc7df1376d79c2e87818
jackbui2014 <jackbui2014@gmail.com>
2021-09-07 17:28:21 +0700

    add reset function

--- index.js
+++ index.js
@@ -156,23 +156,16 @@ const updateRoutes = () => {
 }
 exports.updateRoutes = updateRoutes;
 const removeDir = (pathToDir) =>{
-	if (fs.existsSync(path)) {
-	  const files = fs.readdirSync(path)
-  
-	  if (files.length > 0) {
-		files.forEach(function(filename) {
-		  if (fs.statSync(path + "/" + filename).isDirectory()) {
-			removeDir(path + "/" + filename)
-		  } else {
-			fs.unlinkSync(path + "/" + filename)
-		  }
-		})
-	  } else {
-		console.log("No files found in the directory.")
-	  }
-	} else {
-	  console.log("Directory path not found.")
-	}
+	fs.readdir(directoryPath, function (err, files) {
+	    //handling error
+	    if (err) {
+	        return console.log('Unable to scan directory: ' + err);
+	    } 
+	    //listing all files using forEach
+	    files.forEach(function (file) {
+	    	fs.unlinkSync(file);
+	    });
+	});
   }
 const resetAll = () =>{
 	const directoryPath = './src';
